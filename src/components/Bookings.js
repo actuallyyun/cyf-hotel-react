6fcef567627eb217a6ddffa09a287e38dc33de76
Yun Ji <this.jiyun@gmail.com>
2022-11-10 19:25:58 +0100

    Completed exercises 16-19

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "../data/fakeBookings.json";
 
 const Bookings = () => {
-  const search = searchVal => {
-    console.info("TO DO!", searchVal);
-  };
+  const [bookings, setBookings] = useState([]);
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then(res => res.json())
+      .then(data => setBookings(data));
+  }, []);
+
+  const search = searchVal => {
+    const filteredBookings = bookings.filter(
+      booking =>
+        booking.firstName === searchVal || booking.surname === searchVal
+    );
+    setBookings(filteredBookings);
+    console.log(bookings);
+  };
 
   return (
     <div className="App-content">
